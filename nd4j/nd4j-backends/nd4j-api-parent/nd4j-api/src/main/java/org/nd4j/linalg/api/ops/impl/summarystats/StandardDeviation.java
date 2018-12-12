e8e32ac871bd593a11818fa5e8cb6d4b831b5aaf
Alex Black <blacka101@gmail.com>
2018-12-12 17:33:46 +1100

    Multiple FlatBuffers mapping fixes

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/summarystats/StandardDeviation.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/summarystats/StandardDeviation.java
@@ -88,12 +88,12 @@ public class StandardDeviation extends Variance {
 
     @Override
     public Type getOpType() {
-        return Type.VARIANCE;
+        return Type.SUMMARYSTATS;
     }
 
     @Override
     public Type opType(){
-        return Type.VARIANCE;
+        return Type.SUMMARYSTATS;
     }
 
 
